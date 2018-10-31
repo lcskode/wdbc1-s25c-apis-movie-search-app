586b7b8cfdd9ae437946b746deee748882e42b93
Leo Santillan <lcskode@gmail.com>
2018-10-31 16:53:03 +0800

    Created views folder for ejs to render html templates.

--- app.js
+++ app.js
@@ -21,18 +21,13 @@ app.post("/search", function(req, res){
 
 app.get("/results", function(req, res){
   // hard code imdb first to test 
-  request('http://www.omdbapi.com/?apikey=5d52bbf2&s=california', function(error, response, body){
+  request('http://www.omdbapi.com/?apikey=5d52bbf2&s=star', function(error, response, body){
     // check if there is error fetching data
     if (!error && response.statusCode == 200) {
       // parse body into object and save it in a container
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
       
-      
-      
-      // console.log(parsedData);
-      
-      // console.log(parsedData);
     }
   });
 });
