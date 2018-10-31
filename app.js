da5f2de792ab0914d2fa20a474569c7f4fc69688
Leo Santillan <lcskode@gmail.com>
2018-10-31 16:28:53 +0800

    modified app.js to search for a movie.

--- app.js
+++ app.js
@@ -15,12 +15,26 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.post("/search", function(){
+app.post("/search", function(req, res){
 
 });
 
-app.get("/results", function(){
-
+app.get("/results", function(req, res){
+  // hard code imdb first to test 
+  request('http://www.omdbapi.com/?apikey=5d52bbf2&s=california', function(error, response, body){
+    // check if there is error fetching data
+    if (!error && response.statusCode == 200) {
+      // parse body into object and save it in a container
+      var results = JSON.parse(body);
+      res.send(results["Search"][0]["Title"]);
+      
+      
+      
+      // console.log(parsedData);
+      
+      // console.log(parsedData);
+    }
+  });
 });
 
 
